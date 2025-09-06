74090c229b55cfe27bacf4edee61dff7c23c53e7
veronica <timurkkq@gmail.com>
2025-09-06 11:03:19 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace PROGA
+{   class PROGA
+    { static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //int number = 10;
+            //Console.WriteLine(number);
+            //Console.Write("ВВедите ваше имя: ");
+            //string? name = Console.ReadLine();
+            //Console.WriteLine($"Ваше имя - {name}");
+            //Console.Write("ВВедите ваш возраст: ");
+            //int age = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"Ваш возраст - {age}");
+            int number = 250;
+            byte number2 = (byte)(number + 10);
+            Console.WriteLine(number2);
+            //завести 3 переменные - имя, возраст
+            string name = "chi";
+            int age = 18;
+            string city = "gorod";
+            bool alive = true;
+            bool isDead = false;
+            char symbol = '2';
+            double yNumber = 3.4;
+            float INumber = 3.4F;
+            decimal kNumber = 3.4M;
+            object lNumber = 3.4;
+            var gNumber = 'f';
+            
+        }
+    }
+}
