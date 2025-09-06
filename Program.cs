3c093437e7b6b857d43c8cd0cb403114639bbbe4
veronica <timurkkq@gmail.com>
2025-09-06 11:38:55 +0300

    Лаба

--- Program.cs
+++ Program.cs
@@ -12,23 +12,41 @@ namespace PROGA
             //Console.Write("ВВедите ваш возраст: ");
             //int age = int.Parse(Console.ReadLine());
             //Console.WriteLine($"Ваш возраст - {age}");
-            int number = 250;
-            byte number2 = (byte)(number + 10);
-            Console.WriteLine(number2);
-            //завести 3 переменные - имя, возраст
-            string name = "chi";
-            int age = 18;
-            string city = "gorod";
-            bool alive = true;
-            bool isDead = false;
-            char symbol = '2';
-            double yNumber = 3.4;
-            float INumber = 3.4F;
-            decimal kNumber = 3.4M;
-            object lNumber = 3.4;
-            var gNumber = 'f';
-            int nummmber = 111;
-            
+            //int number = 250;
+            //byte number2 = (byte)(number + 10);
+            //Console.WriteLine(number2);
+            ////завести 3 переменные - имя, возраст
+            //string name = "chi";
+            //int age = 18;
+            //string city = "gorod";
+            //bool alive = true;
+            //bool isDead = false;
+            //char symbol = '2';
+            //double yNumber = 3.4;
+            //float INumber = 3.4F;
+            //decimal kNumber = 3.4M;
+            //object lNumber = 3.4;
+            //var gNumber = 'f';
+            //int nummmber = 111;
+            Console.WriteLine("Задание 1");
+            Console.WriteLine("любимое аниме: ");
+            string name = "devil may cry";
+            Console.WriteLine(name);
+            Console.WriteLine("любимое число: ");
+            int number = 69;
+            Console.WriteLine(number);
+            double pi = Math.PI;
+            Console.WriteLine("любимая буква: ");
+            string bukva = "я";
+            Console.WriteLine(bukva);
+            Console.WriteLine("Задание 2");
+            Console.WriteLine("I need more power!");
+            Console.WriteLine("I need more power!");
+            Console.WriteLine("I need more power!");
+            Console.WriteLine("I need more power!");
+            Console.WriteLine("Задание 3");
+            Console.WriteLine('Hello There');
+
         }
     }
 }
